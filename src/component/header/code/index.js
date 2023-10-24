d2fe67cadccdcde19d13bdea0cd130bd48b1262b
abida128 <abida.nizam11@gmail.com>
2023-10-25 00:58:41 +0500

    This is updated appointment form

---
+++ src/component/header/code/index.js
@@ -0,0 +1,22 @@
+// import React from 'react'
+
+// const index = () => {
+//   return (
+//     <>
+//      <form onSubmit={handleSubmit} >
+//       <ContactPicker name="contact" value={contact} contacts={contactNames} onChange={(e) => setContact(e.target.value)} />
+//       <input name="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+//       <input name="date" type="date" value={date} min={getTodayString()} onChange={(e) => setDate(e.target.value)} />
+//       <input name="time" type="time" value={time} onChange={(e) => setTime(e.target.value)} />
+//       <input type="submit" value="Add Appointment" />
+//     </form> 
+//     </>
+//   )
+// }
+
+// export default index
+
+ {/* <ContactPicker name="contact" value={contact} contacts={contactNames} onChange={(e) => setContact(e.target.value)} />
+      <input name="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <input name="date" type="date" value={date} min={getTodayString()} onChange={(e) => setDate(e.target.value)} />
+      <input name="time" type="time" value={time} onChange={(e) => setTime(e.target.value)} /> */}
