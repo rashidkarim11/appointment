a364ff627511e6879480b7db96063cb7bd7d2c98
abida128 <abida.nizam11@gmail.com>
2023-10-25 01:22:05 +0500

    This is updated appointment form

--- src/pages/appointmentPage/index.js
+++ src/pages/appointmentPage/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function AppointmentPage() {
   return (
-    <div className="px-8 mt-8 text-[poppins]">
+    <div className="px-8 mt-8 mb-4 text-[poppins]">
       <h1 className="text-center">Take An Appointment </h1>
 
       <form className="bg-blue-400 py-10 mx-auto text-center mt-4 border-none border-gray-700 sm:w-3/4 md:w-2/4 lg:w-5/4 py-16">
@@ -14,7 +14,7 @@ function AppointmentPage() {
             type="text"
             placeholder=" Enter your first name"
             required
-            className="border-none rounded border-gray-700 py-2 mt-2"
+            className="border-none rounded border-gray-700 px-4 py-3 mt-2"
           />
         </div>
         <div className="mb-4 flex flex-col px-10">
@@ -25,7 +25,7 @@ function AppointmentPage() {
             type="text"
             placeholder="Enter your last name"
             required
-            className="border-none rounded border-gray-700 py-2 mt-2"
+            className="border-none rounded border-gray-700 px-4 py-3 mt-2"
            
           />
         </div>
@@ -37,7 +37,7 @@ function AppointmentPage() {
             type="email"
             placeholder="Enter your email"
             required
-            className="border-none rounded border-gray-700 py-2 mt-2"
+            className="border-none rounded border-gray-700 px-4 py-3 mt-2"
           />
         </div>
         <div className="mb-4 flex flex-col px-10">
@@ -48,12 +48,12 @@ function AppointmentPage() {
             type="number"
             placeholder="Enter your cell number"
             required
-            className="border-none rounded border-gray-700 py-2 mt-2"
+            className="border-none rounded border-gray-700 px-4 py-3 mt-2"
           />
         </div>
         <button
           type="submit"
-          className="border-none bg-white rounded border-gray-700 px-4 py-2 mt-2"
+          className="border-none bg-white rounded border-gray-700 px-10  py-3 mt-2"
         >
           Submit
         </button>
