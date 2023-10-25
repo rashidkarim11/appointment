349b8cd1e4676bd2a3ec1e5bf71624469539e3a9
mairakhalil73 <mairakhalil73@gmail.com>
2023-10-25 13:51:05 +0000

    updated appointment component

---
+++ src/component/appointmentData/appointmentFormTable.js
@@ -0,0 +1,46 @@
+function AppointmentTableData() {
+    return (
+      <>
+        <div class="ml-5 mt-5 relative overflow-x-auto">
+          <table class="m-auto text-sm text-center text-gray-500 text-gray-400 border">
+            <thead class="text-xs uppercase text-white bg-blue-400">
+              <tr>
+                <th class="p-3">Name</th>
+                <th class="p-3">Email</th>
+                <th class="p-3">Number</th>
+                <th class="p-3">Date & Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="bg-white">
+                <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                  Maira
+                </th>
+                <td class="p-3">maira@gmail.com</td>
+                <td class="p-3">67894456778</td>
+                <td class="p-3">sun 4pm</td>
+              </tr>
+              <tr class="bg-white">
+                <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                  Maira
+                </th>
+                <td class="p-3">maira@gmail.com</td>
+                <td class="p-3">67894456778</td>
+                <td class="p-3">sun 4pm</td>
+              </tr>
+              <tr class="bg-white">
+                <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                  Maira
+                </th>
+                <td class="p-3">maira@gmail.com</td>
+                <td class="p-3">67894456778</td>
+                <td class="p-3">sun 4pm</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
+  export default AppointmentTableData;
+  
