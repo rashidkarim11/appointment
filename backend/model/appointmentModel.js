8d860af16f255550b37abb2380cc44678280b875
mairakhalil73 <mairakhalil73@gmail.com>
2023-10-30 12:52:07 +0000

    updated data types

--- backend/model/appointmentModel.js
+++ backend/model/appointmentModel.js
@@ -6,7 +6,7 @@ const appointmentSchema = new mongoose.Schema({
     required: true,
   },
   date: {
-    type: String,
+    type: Date,
     required: true,
   },
   time: {
@@ -14,7 +14,7 @@ const appointmentSchema = new mongoose.Schema({
     required: true,
   },
   contact: {
-    type: String,
+    type: Number,
     required: true,
   },
 });
